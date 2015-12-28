3b35268e8084bd17e97430f911f2b2b3f7a73c33
Matt <mkreider@gmail.com>
2015-12-27 20:07:43 -0500

    Alien spawn fix

--- lua/PlayerSpawnAdjustments.lua
+++ lua/PlayerSpawnAdjustments.lua
@@ -131,6 +131,62 @@ if Server then
 			self.timeSpinUpStarted = nil		
 		end
 	)
+	
+	// Grab player out of respawn queue unless player passed in (for test framework)
+	function Egg:SpawnPlayer(player)
+
+		PROFILE("Egg:SpawnPlayer")
+
+		local queuedPlayer = player
+		
+		if not queuedPlayer or self.queuedPlayerId ~= nil then
+			queuedPlayer = Shared.GetEntity(self.queuedPlayerId)
+		end
+		
+		if queuedPlayer ~= nil then
+		
+			local queuedPlayer = player
+			if not queuedPlayer then
+				queuedPlayer = Shared.GetEntity(self.queuedPlayerId)
+			end
+		
+			// Spawn player on top of egg
+			local spawnOrigin = Vector(self:GetOrigin())
+			// Move down to the ground.
+			local _, normal = GetSurfaceAndNormalUnderEntity(self)
+			if normal.y < 1 then
+				spawnOrigin.y = spawnOrigin.y - (self:GetExtents().y / 2) + 1
+			else
+				spawnOrigin.y = spawnOrigin.y - (self:GetExtents().y / 2)
+			end
+
+			local gestationClass = self:GetClassToGestate()
+			
+			// We must clear out queuedPlayerId BEFORE calling ReplaceRespawnPlayer
+			// as this will trigger OnEntityChange() which would requeue this player.
+			self.queuedPlayerId = nil
+			
+			local team = queuedPlayer:GetTeam()
+			local success, player = team:ReplaceRespawnPlayer(queuedPlayer, spawnOrigin, queuedPlayer:GetViewAngles(), gestationClass)                
+			player:SetCameraDistance(0)
+			player:SetHatched()
+			// It is important that the player was spawned at the spot we specified.
+			assert(player:GetOrigin() == spawnOrigin)
+			
+			if success then
+			
+				self:TriggerEffects("egg_death")
+				DestroyEntity(self) 
+				
+				return true, player
+				
+			end
+				
+		end
+		
+		return false, nil
+
+	end
 
 end
 
