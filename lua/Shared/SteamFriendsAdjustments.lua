3bfbc97ecd241f0cbde0832a75ce87f54a3f1757
Matt <mkreider@gmail.com>
2016-01-03 22:52:10 -0500

    Fully fixed steam friend eggs and embryos as eggs

--- lua/Shared/SteamFriendsAdjustments.lua
+++ lua/Shared/SteamFriendsAdjustments.lua
@@ -3,13 +3,64 @@
 // lua\Shared\SteamFriendsAdjustments.lua
 // - Dragon
 
-local oldPlayerUI_GetStatusInfoForUnit = PlayerUI_GetStatusInfoForUnit
-function PlayerUI_GetStatusInfoForUnit(player, unit)
+if Client then
 
-    local unitState = oldPlayerUI_GetStatusInfoForUnit(player, unit)
-	if unitState then
-		unitState.IsSteamFriend = (unit:isa("Player") and not GetAreEnemies(player, unit) and unit:GetIsSteamFriend()) or false
+	local oldPlayerUI_GetStatusInfoForUnit = PlayerUI_GetStatusInfoForUnit
+	function PlayerUI_GetStatusInfoForUnit(player, unit)
+
+		local unitState = oldPlayerUI_GetStatusInfoForUnit(player, unit)
+		if unitState then
+			unitState.IsSteamFriend = (unit:isa("Player") and not GetAreEnemies(player, unit) and unit:GetIsSteamFriend()) or false
+		end
+		return unitState
+		
 	end
-	return unitState
+
+	local oldPlayerMapBlipGetMapBlipTeam
+	oldPlayerMapBlipGetMapBlipTeam = Class_ReplaceMethod("PlayerMapBlip", "GetMapBlipTeam",
+		function(self, minimap)
+		
+			local playerTeam = minimap.playerTeam
+			local blipTeam = kMinimapBlipTeam.Neutral
+			local isSteamFriend = false
+			local blipTeamNumber = self:GetTeamNumber()
+			
+			if blipTeamNumber == kMarineTeamType then
+				blipTeam = kMinimapBlipTeam.Marine
+			elseif blipTeamNumber== kAlienTeamType then
+				blipTeam = kMinimapBlipTeam.Alien
+			end		
+			
+			if self.clientIndex and self.clientIndex > 0 and MinimapMappableMixin.OnSameMinimapBlipTeam(playerTeam, blipTeam) then
+
+				local steamId = GetSteamIdForClientIndex(self.clientIndex)
+				if steamId then
+					isSteamFriend = Client.GetIsSteamFriend(steamId)
+				end
+
+			end
+			
+			if not self:GetIsActive() then
+
+				if blipTeamNumber == kMarineTeamType then
+					blipTeam = kMinimapBlipTeam.InactiveMarine
+				elseif blipTeamNumber== kAlienTeamType then
+					blipTeam = kMinimapBlipTeam.InactiveAlien
+				end
+
+			elseif isSteamFriend then
+			
+				if blipTeamNumber == kMarineTeamType then
+					blipTeam = kMinimapBlipTeam.FriendMarine
+				elseif blipTeamNumber== kAlienTeamType then
+					blipTeam = kMinimapBlipTeam.FriendAlien
+				end
+				
+			end  
+
+			return blipTeam
+		
+		end
+	)
 	
 end
