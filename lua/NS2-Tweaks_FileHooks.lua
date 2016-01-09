8aaa4df73168304accfd45e7cc18ffbe3cc8d4af
Matt <mkreider@gmail.com>
2016-01-09 03:47:20 -0500

    Added filehooks entry point

---
+++ lua/NS2-Tweaks_FileHooks.lua
@@ -0,0 +1,8 @@
+// Natural Selection 2 'Tweaks' Mod
+// Source located at - https://github.com/xToken/NS2-Tweaks
+// lua\NS2-Tweaks_FileHooks.lua
+// - Dragon
+
+//Filehooks
+//Add in manually.
+ModLoader.SetupFileHook( "lua/Door.lua", "lua/Replace/DoorAdjustments.lua", "replace" )
