18a8f684e7d1abc98cf6fe21b3d457910fd67405
Matt <mkreider@gmail.com>
2016-01-16 01:53:00 -0500

    File location update
    Fix for construct progress ghosting.

--- lua/NS2-Tweaks_FileHooks.lua
+++ lua/NS2-Tweaks_FileHooks.lua
@@ -7,6 +7,8 @@
 //Add in manually.
 //Better Doors
 ModLoader.SetupFileHook( "lua/Door.lua", "lua/Replace/Door.lua", "replace" )
+//Fix for buildfraction gitter.
+ModLoader.SetupFileHook( "lua/ConstructMixin.lua", "lua/Post/ConstructMixin.lua", "post" )
 //Predict Collision fixes 'lib' needs to know when Predict VM is fully loaded.  No such callback exists afaik.
 if Predict then
 	ModLoader.SetupFileHook( "lua/PostLoadMod.lua", "lua/Predict/predict_loaded.lua", "post" )
