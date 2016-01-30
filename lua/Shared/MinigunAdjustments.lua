2ba6731e339b159a0b1da3dac54155f9979a57a5
Matt <mkreider@gmail.com>
2016-01-29 20:05:04 -0500

    Minigun overheat fix? lol

---
+++ lua/Shared/MinigunAdjustments.lua
@@ -0,0 +1,6 @@
+// Natural Selection 2 'Tweaks' Mod
+// Source located at - https://github.com/xToken/NS2-Tweaks
+// lua\CompMod\Shared\MinigunAdjustments.lua
+// - Dragon
+
+Shared.LinkClassToMap("Minigun", Minigun.kMapName, { heatAmount = "float (0 to 1 by 0.001)" })
